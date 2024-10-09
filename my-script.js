695935bd6a88dfa26a161e544d1aeef59b9cd99d
codingwithroman <r.knoef@hotmail.com>
2024-10-09 11:57:18 +0200

    Add function for generating interactive grid
    Function that dynamically generates a grid of divs based on input number and adds it to the DOM, then use an event listener for mouseover event to turn background color of divs black, the event listener is attached to the parent element that already exists and has a conditional statement to check if dynamic elements have been added.

--- my-script.js
+++ my-script.js
@@ -1,11 +1,58 @@
 const mainContainer = document.getElementById("mainContainer");
 
-function generateGrid(e) {
-    let gridDimension = 16;
-    let gridContent = gridDimension * gridDimension;
+function generateGrid(heightWidth) {
+    let gridDimension;
 
-    for (let i = 0; i < gridContent; i++) {
+    if (heightWidth) {
+        gridDimension = heightWidth;
+    } else {
+        gridDimension = 16;
+    }
+
+    let containerSize = 650 / gridDimension;
+
+    for (let i = 0; i < gridDimension * gridDimension; i++) {
         const square = document.createElement("div");
-        square.classList.add("square")
+        square.classList.add("square");
+        square.style.width = `${containerSize}px`;
+        square.style.height = `${containerSize}px`;
+        
+        mainContainer.appendChild(square);
     }
 }
+
+generateGrid();
+
+const squares = document.getElementsByClassName("square");
+
+
+
+// Array.from(squares).forEach(function(div) {
+//     div.addEventListener("mouseover", (event) => {
+//         div.style.backgroundColor = "black";
+//         console.log(event);
+//     })
+// })
+
+mainContainer.addEventListener("mouseover", function(event) {
+    if (event.target.classList.contains("square")) {
+        event.target.style.backgroundColor = "black";
+    }
+})
+
+
+
+const buttons = document.querySelectorAll("button");
+
+
+//make buttons delete current grid and generate a grid that's as high and wide as the input of prompt
+Array.from(buttons).forEach(function(button) {
+    button.addEventListener("click", () => {
+        document.querySelectorAll(".square").forEach(div => div.remove());
+        
+        const heightWidth = prompt("Type a number 0-100:");
+        
+        generateGrid(heightWidth);
+    })
+})
+
